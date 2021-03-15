6af20316f08bb31314a08a55dacdea3f4dc8d22e
malhill <74620676+malhill@users.noreply.github.com>
2021-03-14 23:47:43 -0400

    completed seed and schema db

--- db/employee_tracker.sql
+++ db/employee_tracker.sql
@@ -4,17 +4,15 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
-  id INTEGEREGER AUTO_INCREMENT PRIMARY KEY,
-  dep_name VARCHAR(30) NOT NULL,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dep_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INTEGER  AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL  NOT NULL,
-  dep_id INTEGER  NOT NULL,
-  INDEX dep_ind (department_id),
-  CONSTRAINTEGER fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+  dep_id INTEGER  NOT NULL
 );
 
 CREATE TABLE employee (
@@ -22,5 +20,5 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER NOT NULL,
-  manager_id INTEGER,
+  man_id INTEGER
 );
